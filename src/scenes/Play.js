1ef6fb3e89def58a3f527cd14f9e9ba62b0b8e9a
Jonathan Lewis <jontyjlewis@gmail.com>
2022-04-08 15:44:53 -0700

    added scrolling starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,16 +12,20 @@ class Play extends Phaser.Scene {
     create() {
         //this.add.text(20, 20, "this is the play scene");
         
-        // border rectangle
+        // starfield background
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
+        // green UI rectangle
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
+        // white border
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    }
 
-        // starfield background
-        
-
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
