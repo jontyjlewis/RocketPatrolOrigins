3be53237491b3d055fa18b2b77ad1b63546fb380
Jonathan Lewis <jontyjlewis@gmail.com>
2022-04-09 14:58:21 -0700

    Added Rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,10 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+        
     }
 
     create() {
@@ -23,6 +26,9 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
     }
 
     update() {
