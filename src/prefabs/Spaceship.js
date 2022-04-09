8c314704414a14bf369491507ffbc41c223701da
Jonathan Lewis <jontyjlewis@gmail.com>
2022-04-09 15:29:28 -0700

    added enemy ships

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,20 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);   // adds to the existing scene
+        this.points = pointValue;   // stores the pointValue
+        this.moveSpeed = 3;         // ship speed
+    }
+
+    update() {
+        // have spaceship move across the screen right to left
+        this.x -= this.moveSpeed;
+
+        // wrap ship around once off screen
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
